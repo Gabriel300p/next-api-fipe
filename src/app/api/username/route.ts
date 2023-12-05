2803c009cf3cf52697452a76ab0601f0958ecf85
Fulano de Tal <gabriel.andrade200p@gmail.com>
2023-12-05 10:54:53 -0300

    initial

--- src/app/api/username/route.ts
+++ src/app/api/username/route.ts
@@ -1,23 +1,11 @@
 import { db } from "@/lib/db";
-import { z } from "zod";
 
 export async function POST(req: Request) {
-  const bodySchema = z.object({
-    email: z.string(),
+  const body = await req.json();
+  const user = await db.user.findFirst({
+    where: {
+      email: body.email,
+    },
   });
-
-  const { email } = bodySchema.parse(req.json);
-
-  try {
-    const user = await db.user.findFirst({
-      where: {
-        email: email,
-      },
-    });
-    return new Response(JSON.stringify(user), { status: 200 });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new Response(error.message, { status: 422 });
-    }
-  }
+  return new Response(JSON.stringify(user), { status: 200 });
 }
